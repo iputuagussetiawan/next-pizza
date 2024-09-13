1579ef8f0727aea8e0f018088887f342e00d9809
I Putu Agus Setiawan <agussetiawaniputu@gmail.com>
2024-09-13 22:52:44 +0800

    ui.shadcn integration

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,11 @@
+import { Button } from "@/components/ui/button";
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <h1>Home</h1>
+    <>
+      <h1>Home</h1>
+      <Button variant={"outline"}>Submit</Button>
+    </>
   );
 }
---
+++ installation.md
@@ -0,0 +1,7 @@
+1. First Setup 
+ npx create-next-app@latest next-pizza --typescript --tailwind --eslint
+
+2. Add Shadcn UI
+https://ui.shadcn.com/docs/installation/next
+npx shadcn@latest init
+npx shadcn@latest add button
