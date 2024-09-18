b03e79a3359d2d62af066a9e9763ec479f307172
I Putu Agus Setiawan <agussetiawaniputu@gmail.com>
2024-09-18 11:18:57 +0800

    add filter by ingredients

--- components/shared/flters.tsx
+++ components/shared/flters.tsx
@@ -1,4 +1,7 @@
 
+import { Input } from "../ui/input";
+import { RangeSlider } from "../ui/range-slider";
+import { CheckboxFiltersGroup } from "./checkbox-filters-group";
 import { FilterCheckbox } from "./filter-checkbox";
 import { Title } from "./title";
 
@@ -17,8 +20,61 @@ export const Filters:React.FC<Props>=({className})=>{
             </div>
 
             <div className="mt-5 border-y border-y-neutral-100 py-6 pb-7">
-                Testeeste
+                <p className="font-bold mb-3">Price from and to:</p>
+                <div className="flex gap-3 mb-5">
+                    <Input type="number" placeholder="0" min={0} max={30000} defaultValue={0}/>
+                    <Input type="number"  min={100} max={30000} placeholder="1000"/>
+                </div>
+                <RangeSlider min={100} max={500} step={10} value={[0,5000]} />
             </div>
+            <CheckboxFiltersGroup 
+                title="Ingredients:"
+                className="mt-5"
+                limit={3}
+                defaultItems={[
+                    {
+                        text:'Cheese sauce',
+                        value:'1'
+                    },
+                    {
+                        text:'Mozzarella',
+                        value:'2'
+                    },
+                    {
+                        text:'Garlic',
+                        value:'3'
+                    },
+                ]}
+
+                items={[
+                    {
+                        text:'Cheese sauce',
+                        value:'1'
+                    },
+                    {
+                        text:'Mozzarella',
+                        value:'2'
+                    },
+                    {
+                        text:'Garlic',
+                        value:'3'
+                    },
+                    {
+                        text:'Pickled cucumbers',
+                        value:'4'
+                    },
+                    {
+                        text:'Red onion',
+                        value:'5'
+                    },
+                    {
+                        text:'Tomatoes',
+                        value:'6'
+                    },
+
+                    
+                ]}
+            />
         </div>
     );
 };
