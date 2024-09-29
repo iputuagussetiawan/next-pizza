388aee0f8db9b036a022f3bc8dda89d7ac356324
I Putu Agus Setiawan <agussetiawaniputu@gmail.com>
2024-09-29 19:58:21 +0800

    fix error on price

--- components/shared/products-group-list.tsx
+++ components/shared/products-group-list.tsx
@@ -35,21 +35,21 @@ export const ProductsGroupList: React.FC<Props> = ({
             setActiveId(categoryId);
             console.log(title, categoryId);
         }
-    }, [categoryId,title,intersection?.isIntersecting]);
+    }, [categoryId,title,intersection?.isIntersecting, setActiveId]);
     return (
         <div className={className} id={title} ref={intersectionRef}>
             <Title text={title} size="lg" className="font-extrabold mb-5" />
             <div className={cn('grid grid-cols-3 gap-[50px]', listClassName)}>
                 {products.map((product) => {
                     // Safely check if product.items exists and has elements
-                    const price = product.items?.[0]?.price || 'N/A'; // Handle missing price
+                    //const price = Number(product.items?.[0]?.price) || 0; // Handle missing price
                     return (
                         <ProductCard
                             key={product.id}
                             id={product.id}
                             name={product.name}
                             imageUrl={product.imageUrl}
-                            price={price}
+                            price={200}
                         />
                     );
                 })}
